93d24d571a17f1e3c86db0ba49d75af5d01d61e4
grm1405 <grmrodri@gmail.com>
2025-07-21 00:04:08 -0500

    scss properties

--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
@@ -10,20 +10,43 @@ import { StorageService } from '../services/storage.service';
   templateUrl: './intro.page.html',
   styleUrls: ['./intro.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class IntroPage implements OnInit {
+
+  slides = [
+    {
+      title: 'Bienvenido',
+      description: 'Explora géneros musicales únicos',
+      image: 'https://cdn-icons-png.flaticon.com/512/4072/4072730.png'
+    },
+    {
+      title: 'Personaliza tu tema',
+      description: 'Elige entre claro y oscuro',
+      image: 'https://cdn-icons-png.flaticon.com/512/1828/1828490.png'
+    },
+    {
+      title: 'Guarda tu progreso',
+      description: 'Todo se recuerda incluso si cierras la app',
+      image: 'https://cdn-icons-png.flaticon.com/512/744/744465.png'
+    },
+    {
+      title: '¡Comienza ya!',
+      description: 'Presiona el botón para ir al home',
+      image: 'https://cdn-icons-png.flaticon.com/512/709/709496.png'
+    }
+  ];
 
   constructor(
     private router: Router,
     private storageService: StorageService
-  ) { }
+  ) {}
 
   ngOnInit() {}
 
   async goBack() {
-    await this.storageService.set('vistoIntro', true);
-    console.log("Intro vista, volviendo al Home");
+    await this.storageService.set('intro-visto', true);
     this.router.navigateByUrl('/home');
   }
 }
